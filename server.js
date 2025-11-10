2c6469db58a5ca8336dfc6789a08421faac9cdaa
maryosborne1221 <mary@mscodingacademies.org>
2025-11-10 16:11:25 -0600

    started on route and set up error handling

--- server.js
+++ server.js
@@ -7,9 +7,21 @@ const PORT = process.env.PORT || 3000
 const helmet = require('helmet')
 const cors = require('cors')
 
-server.use(helmet())
-server.use(cors())
+//configuring helmet
+//server.use(helmet())
+server.use(helmet.contentSecurityPolicy({
+    useDefaults: true,
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    directives: {
+        "img-src": ["'self'", "https: data"],
+        "scriptSrc": ["'self'", "cdn.jsdeliver.net"]
+    }
+}))
 
+server.use(cors())
+server.use(express.json())
+server.use(express.urlencoded({ extended: true}))
 const router = require('./routes/router')
 
 server.listen(PORT, ()=> console.log('The Dodgers won the 2025 World Series!!'))
