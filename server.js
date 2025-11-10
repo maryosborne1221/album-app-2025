7b4dee979af434ff2fd31ad7fda4f7edd5ba49a1
maryosborne1221 <mary@mscodingacademies.org>
2025-11-10 13:51:51 -0600

    importing router

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 3000
 
+//Handle security
+const helmet = require('helmet')
+const cors = require('cors')
+
+server.use(helmet())
+server.use(cors())
+
+const router = require('./routes/router')
+
 server.listen(PORT, ()=> console.log('The Dodgers won the 2025 World Series!!'))
+
